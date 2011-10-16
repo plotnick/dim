a6db5badd056c4d4a8b7082e8f33d6203354e97b
Alex Plotnick <shrike@netaxs.com>
2011-10-16 12:39:18 -0400

    Search for event handlers using the MRO.

--- event.py
+++ event.py
@@ -19,7 +19,7 @@ def handler(event_classes):
     exception. In that case, the next registered handler (or the default handler
     if no more handlers are available) will be invoked."""
     def set_handler(method):
-        method.__handler_for__ = event_classes \
+        method.handler_for = event_classes \
             if isinstance(event_classes, (list, tuple)) \
             else (event_classes,)
         return method
@@ -32,11 +32,8 @@ class EventHandlerClass(type):
         cls = super(EventHandlerClass, metaclass).__new__(metaclass, name,
                                                           bases, namespace)
         cls.__handlers__ = {}
-        for base in bases:
-            for ev, handlers in getattr(base, "__handlers__", dict()).items():
-                cls.__handlers__[ev] = cls.__handlers__.get(ev, []) + handlers
         for obj in filter(callable, namespace.values()):
-            for ev in getattr(obj, "__handler_for__", ()):
+            for ev in getattr(obj, "handler_for", ()):
                 cls.__handlers__[ev] = [obj] + cls.__handlers__.get(ev, [])
         return cls
 
@@ -46,18 +43,22 @@ class EventHandler(object):
     __metaclass__ = EventHandlerClass
 
     def handle_event(self, event):
-        """Dispatch an event to the registered handler. Subclasses may, but
-        generally should not, override this method."""
-        try:
-            handlers = self.__handlers__[type(event)]
-        except KeyError:
-            return self.unhandled_event(event)
-        for handler in handlers:
+        """Dispatch an event to the most-specific compatible handler.
+
+        This method searches for handlers using the method resolution order.
+        Subclasses may, but generally should not, override this method."""
+        event_class = type(event)
+        for cls in self.__class__.__mro__:
             try:
-                return handler(self, event)
-            except UnhandledEvent:
-                # Handler declined to handle the event; try the next one.
-                pass
+                handlers = cls.__handlers__[event_class]
+            except (AttributeError, KeyError):
+                continue
+            for handler in handlers:
+                try:
+                    return handler(self, event)
+                except UnhandledEvent:
+                    # Handler declined to handle the event; try the next one.
+                    pass
         else:
             return self.unhandled_event(event)
 
