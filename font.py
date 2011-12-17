522821c235e691b3eda892179aed8af7c0cfacfb
Alex Plotnick <shrike@netaxs.com>
2011-12-17 11:05:21 -0500

    Implemented proper text_width function.

--- font.py
+++ font.py
@@ -1,5 +1,6 @@
 # -*- mode: Python; coding: utf-8 -*-
 
+from array import array
 import logging
 
 import xcb
@@ -40,5 +41,36 @@ class FontCache(object):
         return self.font_info[font]
 
 def text_width(font_info, string):
-    # This implementation is only temporary.
-    return font_info.max_bounds.character_width * len(string)
+    """Using the character metrics in font_info, compute and return the width
+    of the given string."""
+    def nonexistent_char(char_info):
+        return (char_info.character_width == 0 and
+                char_info.left_side_bearing == 0 and
+                char_info.right_side_bearing == 0 and
+                char_info.ascent == 0 and
+                char_info.descent == 0)
+
+    min_byte1 = font_info.min_byte1
+    min_char_or_byte2 = font_info.min_char_or_byte2
+    max_char_or_byte2 = font_info.max_char_or_byte2
+    d = max_char_or_byte2 - min_char_or_byte2 + 1
+    def get_char_info(row, col, default):
+        try:
+            info = font_info.char_infos[((row - min_byte1) * d) +
+                                        (col - min_char_or_byte2)]
+        except IndexError:
+            info = None
+        return info if info and not nonexistent_char(info) else default
+
+    default = get_char_info(font_info.default_char >> 8,
+                            font_info.default_char & 0xff,
+                            None)
+    if (default and
+        (font_info.min_bounds.character_width ==
+         font_info.max_bounds.character_width)):
+        return font_info.min_bounds.character_width * len(string)
+
+    string16 = array("B", unicode(string).encode("UTF-16BE"))
+    infos = (get_char_info(string16[i], string16[i+1], default)
+             for i in xrange(0, len(string16), 2))
+    return sum(info.character_width for info in infos if info)
